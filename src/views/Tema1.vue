fe630fdc7854c440992ef4487c8b9608d8590849
elrilacho <camilobr17@gmail.com>
2022-08-30 16:41:57 -0500

    Tema1 listo

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -90,28 +90,134 @@
     Separador 
     #t_1_2.titulo-segundo.color-primario(data-aos='fade-right')
       h2 1.2  #[i Use of “would”] / (Uso de “would”)
-        
+    .row.justify-content-center.mb-5
+      .col-lg-2
+        img(src='@/assets/curso/temas/9.svg', alt='')  
+      .col-lg-10 
+        .contenedor-traductor
+          .contenedor-traductor__eng
+            p.fst-italic The use of “would” is really important in English because it is necessary when talking about hypothetical situations. Would is an auxiliary verb that modifies the verb, so you don’t have to conjugate the verb. Let’s see how to use it in different grammatical structures:
+          .contenedor-traductor__esp(:class="{ mostrar: ocultarEsp}")
+            p 
+              span.r-5 
+              //todo! AGREGAR TRADUCCION 
 
+          a.boton-traduccion(@click='ocultarEsp = !ocultarEsp') Ver traducción
+    .row.justify-content-center.mb-5
+      .col-lg-3.mb-lg-0.mb-3
+        img(src='@/assets/curso/temas/10.png', alt='')
+      .col-lg-9
+        AcordionA.audios-colores.mb-5(tipo="a" clase-tarjeta="tarjeta tarjeta-3")
+          div(titulo="<em>Affirmative structure</em> / Estructura afirmativa").p-5
+            .tarjeta.bg-white.p-4
+              p.text-center
+              img.mb-4(src='@/assets/curso/temas/11.svg', alt='')
+              .row.justify-content-center.mb-3
+                .col-lg-3.mb-lg-0.mb-3: img(src='@/assets/curso/temas/12.svg', alt='')
+                .col-lg-6
+                  TarjetaAudio.color-primario.bg-white.mb-3(
+                    texto="<span class='f-1'>Sam </span><span class='f-2'>would </span><span class='f-3'>travel to </span><span class='f-4 text-white'>China. </span>"
+                    :audio="require('@/assets/audios/tema1/1.wav')"
+                    )
+          div(titulo="<em>Negative structure</em> / Estructura negativa").p-5
+            .tarjeta.bg-white.p-4
+              p.text-center
+              img.mb-4(src='@/assets/curso/temas/13.svg', alt='')
+              .row.justify-content-center.mb-3
+                .col-lg-3.mb-lg-0.mb-3: img(src='@/assets/curso/temas/14.svg', alt='')
+                .col-lg-6
+                  TarjetaAudio.color-primario.bg-white.mb-3(
+                    texto="<span class='f-1'>Sam </span><span class='f-2'>would not   </span><span class='f-3'>travel to  </span><span class='f-4 text-white'>China. </span>"
+                    :audio="require('@/assets/audios/tema1/1.wav')"
+                    )
+                  p.ms-3 Sam no viajaría a China.
+          div(titulo="<em>Interrogative structures</em> / Estructuras interrogativas").p-5
+            .tarjeta.bg-white.p-4
+              p.text-center
+              img.mb-4(src='@/assets/curso/temas/15.svg', alt='')
+              .row.justify-content-center.mb-3
+                .col-lg-3.mb-lg-0.mb-3: img(src='@/assets/curso/temas/16.svg', alt='')
+                .col-lg-6
+                  TarjetaAudio.color-primario.bg-white.mb-3(
+                    texto="<span class='f-2'>would </span><span class='f-1'>Sam </span><span class='f-3'>travel to </span><span class='f-4 text-white'>China? </span>"
+                    :audio="require('@/assets/audios/tema1/1.wav')"
+                    )
+                  p.ms-3 ¿Viajaría Sam a China?
+          div(titulo="<em>Informative questions</em> / Preguntas informativas").p-5
+            .tarjeta.bg-white.p-4
+              p.text-center
+              img.mb-4(src='@/assets/curso/temas/17.svg', alt='')
+              .row.justify-content-center.mb-3
+                .col-lg-3.mb-lg-0.mb-3: img(src='@/assets/curso/temas/18.svg', alt='')
+                .col-lg-6
+                  TarjetaAudio.color-primario.bg-white.mb-3(
+                    texto="<span class='f-5'>When </span><span class='f-2'>would</span><span class='f-1'>Sam </span><span class='f-3 '>travel to </span><span class='f-4 text-white'>China? </span>"
+                    :audio="require('@/assets/audios/tema1/1.wav')"
+                    )
+    .row.justify-content-center.mb-5
+      .col-lg-7.mb-lg-0.mb-3
+        .contenedor-traductor
+          .contenedor-traductor__eng
+            p.fst-italic The structures are not different from most of the grammar tenses based on auxiliary verbs. In the affirmative sentences, “would” is placed before the verb and this last one is always in base form. For the negative form, a “not” is placed between “would” and the verb; the contraction of “would not” is “wouldn’t”, in case you want the sentences shorter. In interrogative forms, we just place the “would” at the beginning. 
+              br
+              | Next, we will see the second conditional structure:
+          .contenedor-traductor__esp(:class="{ mostrar: ocultarEsp}")
+            p 
+              span.r-5 
+              //todo! AGREGAR TRADUCCION 
 
+          a.boton-traduccion(@click='ocultarEsp = !ocultarEsp') Ver traducción
 
+      .col-lg-5
+        img(src='@/assets/curso/temas/19.jpg', alt='')
+    .row.justify-content-center.mb-5
+      .col-lg-7
+        .titulo-sexto.color-primario(data-aos='fade-right')
+          h5 Figura 1
+          span #[i Second Conditional Structures]  / (Estructuras del segundo condicional)
+        img(src='@/assets/curso/temas/20.svg', alt='')
+    p 
+      span.fst-italic.fw-normal.r-3 The second conditional refers to speculative situations. They are not directly connected to reality; they are just a supposition of what the person would do or what would happen under those situations. 
+      span.text-gray / El segundo condicional se refiere a situaciones especulativas. No están directamente conectadas con la realidad; son solo una suposición de lo que la persona haría o lo que sucedería bajo esas situaciones.
+      br
+      br
+      span.fst-italic.fw-normal.r-3 For example: 
+      | / Por ejemplo:
+    .row.justify-content-center.mb-5
+      .col-lg-4
+        .tarjeta.color-acento-contenido.p-4.hbg--4.h-100
+          img.mb-3(src='@/assets/curso/temas/21.png', alt='')
+          .tarjeta.bg-white.border-1.p-3.d-flex.flex-column.justify-content-center
+                Audio.color-primario.d-block(:audio="require('@/assets/audios/tema1/1.wav')").m-auto.mb-2
+                p.text-center
+                  span.r-1.fst-italic If I had a million dollars, I would buy a big house.
 
-
-
-
-
-
-
-
-
-
-
-
-
+                  br 
+                  | Si yo tuviera un millón de dólares, compraría una casa grande.
+      .col-lg-4
+        .tarjeta.color-acento-contenido.p-4.hbg--4.h-100
+          img.mb-3(src='@/assets/curso/temas/22.png', alt='')
+          .tarjeta.bg-white.border-1.p-3.d-flex.flex-column.justify-content-center
+                Audio.color-primario.d-block(:audio="require('@/assets/audios/tema1/1.wav')").m-auto.mb-2
+                p.text-center
+                  span.r-1.fst-italic I would become a basketball player if I were taller.
+                  br 
+                  | Me convertiría en jugador de baloncesto si yo fuera más alto.
+      .col-lg-4
+        .tarjeta.color-acento-contenido.p-4.hbg--4.h-100
+          img.mb-3(src='@/assets/curso/temas/23.png', alt='')
+          .tarjeta.bg-white.border-1.p-3.d-flex.flex-column.justify-content-center
+                Audio.color-primario.d-block(:audio="require('@/assets/audios/tema1/1.wav')").m-auto.mb-2
+                p.text-center
+                  span.r-1.fst-italic If I did more exercise, I would be in shape.
+                  br 
+                  | Si yo hiciera más ejercicio, yo estaría en forma.
+    p Finalmente, de acuerdo con la temática explicada, escuche el siguiente diálogo y complete las palabras faltantes:
 
     Dialogo.color-acento-contenido(:dialogo="dialogo")
-      template(#nombre-actividad) Arrastrar y Soltar
+      template(#nombre-actividad) Arrastrar y soltar
       template(#titulo-actividad) Completa la conversación
-      template(#descripcion-actividad)
+      template(#descripcion-actividad) Arrastra y suelta cada una de las palabras en el orden correcto.
 </template>
 
 <script>
@@ -123,69 +229,53 @@ export default {
     dialogo: {
       personajes: [
         {
-          nombre: 'Ana',
-          img: require('@/assets/curso/temas/ana.svg'),
+          nombre: 'Carlo',
+          img: require('@/assets/curso/temas/carlo.svg'),
         },
         {
-          nombre: 'Luisa',
-          img: require('@/assets/curso/temas/luisa.svg'),
+          nombre: 'Dave',
+          img: require('@/assets/curso/temas/dave.svg'),
         },
       ],
       dialogo: [
         {
-          personaje: 'Ana',
-          textoIng: 'Hi! How ***?',
+          personaje: 'Carlo',
+          textoIng: 'Hey Dave! Where would you *** if you had an airplane?',
           textoEsp: 'Pendiente traduccion',
           audio: require('@/assets/audios/a1/1.wav'),
           palabra: 'have you been',
         },
         {
-          personaje: 'Luisa',
-          textoIng: 'Hey! Awesome! How about you?',
+          personaje: 'Dave',
+          textoIng: ' I think I *** fly around Europe.',
           textoEsp: '',
           audio: require('@/assets/audios/a1/2.wav'),
         },
         {
-          personaje: 'Ana',
-          textoIng: 'I’m great! *** from my trip yesterday',
+          personaje: 'Carlo',
+          textoIng: 'Wow! I wouldn’t do that; I am not into flying much.',
           textoEsp: '',
           audio: require('@/assets/audios/a1/3.wav'),
-          palabra: 'I arrived',
         },
         {
-          personaje: 'Luisa',
-          textoIng: 'Really? Where did you go?',
+          personaje: 'Dave',
+          textoIng: 'So, what would you do if you had a million dollars?',
           textoEsp: '',
           audio: require('@/assets/audios/a1/4.wav'),
         },
         {
-          personaje: 'Ana',
+          personaje: 'Carlo',
           textoIng:
-            '*** to Italy. I have been there many times, but I love it.',
+            'Well, if I *** a millionaire, I *** pay my debts and then I would *** a few houses to rent.',
           textoEsp: '',
           audio: require('@/assets/audios/a1/5.wav'),
           palabra: 'I went',
         },
         {
-          personaje: 'Luisa',
-          textoIng:
-            'Wow! *** there for the first time 2 years ago and I loved it too.',
+          personaje: 'Dave',
+          textoIng: 'You are such a businessman!',
           textoEsp: '',
           audio: require('@/assets/audios/a1/6.wav'),
-          palabra: 'I traveled',
-        },
-        {
-          personaje: 'Ana',
-          textoIng: 'Sure! Have you already planned your next trip?',
-          textoEsp: '',
-          audio: require('@/assets/audios/a1/7.wav'),
-        },
-        {
-          personaje: 'Luisa',
-          textoIng: 'I haven’t, but *** many options this time.',
-          textoEsp: '',
-          audio: require('@/assets/audios/a1/8.wav'),
-          palabra: 'I have seen',
         },
       ],
     },
